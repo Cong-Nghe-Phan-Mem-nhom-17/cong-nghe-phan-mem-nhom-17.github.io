39f4e0683c5521572e29ef8d98bd41870008c3db
gnuh-naut <traisocson123@gmail.com>
2020-04-07 23:37:41 +0700

    phase 1 phase 2 scene 2

--- my-game/Menu.js
+++ my-game/Menu.js
@@ -1,73 +1,109 @@
 
 
 class Menu extends Phaser.Scene {
+
     constructor() {
+
         super('Menu')
+
     }
 
     preload() {
+
         this.load.image('backGround', 'assets/images/backGround.png')
         this.load.image('frameMenu', 'assets/images/frameMenu.png')
         this.load.image('play1', 'assets/images/play1.png')
         this.load.image('play2', 'assets/images/play2.png')
         this.load.image('play3', 'assets/images/play3.png')
         this.load.audio('audioBelow', 'assets/audio/below.mp3')
+
     }
     create(){
+        
         this.background = this.add.image(0, 0, "backGround").setOrigin(0, 0);
+
         this.framework = this.add.image(233, 115, "frameMenu").setOrigin(0, 0);
+
         // this.audioBelow = this.add.audio('audioBelow')
          
 
-        this.text0 = this.add.text(350, 200, "CHOOSE A GAME TO PLAY!", {font: "50px Arial", fill: "red"}).setOrigin(0, 0);
+        this.text0 = this.add.text(350, 200, "CHOOSE A GAME TO PLAY!", {
+
+            font: "50px Arial", 
+            fill: "red"
+
+        }).setOrigin(0, 0);
+
         // Choose scene 1 
         this.play1 = this.add.sprite(350, 300, 'play1').setOrigin(0,0)
 
         this.play1.setInteractive().on('pointerdown', () => {
+
             _const1 = 360;
             ballNumber1 = 9;
             status1 = 0;
             this.scene.start('Scene1')
+
         })
 
         this.textPlay1 = this.add.text(360, 550, "           Identify \n    above and below ", {
+
             font: "20px Arial", 
             fill: "black"
+
         }).setOrigin(0, 0)
 
         this.textPlay1.setInteractive().on('pointerdown', () =>{
+
             _const1 = 360
             ballNumber1 = 9
             status1 = 0
             this.scene.start('Scene1')
+
         })
+
         // Choose scene 2
+
         this.play2 = this.add.sprite(650, 300, 'play2').setOrigin(0,0)
 
         this.play2.setInteractive().on('pointerdown', () => {
+
             _const2 = 360;
             ballNumber2 = 9;
             status2 = 0;
             this.scene.start('Scene2')
+
         })
 
         this.textPlay2 = this.add.text(670, 550, "Say: above or below", {
+
             font: "20px Arial",
             fill: "black"
+
         }).setOrigin(0, 0)
 
         this.textPlay2.setInteractive().on('pointerdown', () => {
+
             _const2 = 360
             ballNumber2 = 9
             status2 = 0
             this.scene.start('Scene2')
+
         })
+
         // Choose scene 3
+
         this.play3 = this.add.sprite(950, 300, 'play3').setOrigin(0,0)
 
+        // this.play3.setInteractive().on('pointerdown', () => {
+        //     this.scene.start('Scene3')
+        // })
+
         this.textPlay3 = this.add.text(950, 550, "Place an object in a given \nposition above below \nanother object", {
+
             font: "20px Arial",
             fill: "black"
+
         }).setOrigin(0, 0)
 
         // this.textPlay3.setInteractive().on('pointerdown', () => {
@@ -75,11 +111,17 @@ class Menu extends Phaser.Scene {
         // })
 
         this.input.on('gameobjectover', function (pointer, gameObject) {
+
             gameObject.setTint(0xE973CF);
+
         });
+
         this.input.on('gameobjectout', function (pointer, gameObject) {
+
             gameObject.clearTint();
+            
         });
+
     }
 
     update() {
--- my-game/Scene2.js
+++ my-game/Scene2.js
@@ -3,6 +3,7 @@ let ballNumber2 = 9;
 let status2 = 0;
 
 
+
 class Scene2 extends Phaser.Scene{
 
     constructor() {
@@ -30,15 +31,6 @@ class Scene2 extends Phaser.Scene{
         // Object of the game;
         this.background = this.add.image(0, 0, "backGround").setOrigin(0, 0);
         this.framework = this.add.image(233, 115, "frameWork").setOrigin(0, 0);
-        this.tree = this.add.image(250, 270, "tree").setOrigin(0, 0);
-        this.cat = this.add.image(250, 230, "cat").setOrigin(0, 0);
-        this.bird = this.add.image(340, 560, "bird").setOrigin(0, 0);
-        this.squirrel = this.add.image(350, 400, "squirrel").setOrigin(0, 0);
-
-        this.ok1 = this.add.image(1105, 362, "ok1").setOrigin(0, 0);
-        this.ok1 = this.add.image(1105, 420, "ok1").setOrigin(0, 0);
-
-        //this.ok2 = this.add.image(1105, 362, "ok2").setOrigin(0, 0);
 
         this.backButton = this.add.sprite(245, 117, 'backButton').setOrigin(0,0);
 
@@ -52,27 +44,119 @@ class Scene2 extends Phaser.Scene{
             gameObject.clearTint();
         })
 
-
-
         // ball;
-        var arr = new Array("redBall");
+        arr = new Array("redBall");
         for(var i = 0; i < ballNumber2; i++){
             arr[i] = this.add.image(_const2 += 70, 137, "redBall").setOrigin(0, 0);
         }
+
+       
+        this.phase_1()
+
+    }
+
+    phase_1() {
+
+        this.tree = this.add.image(250, 250, "tree").setOrigin(0, 0);
+        this.cat = this.add.sprite(250, 230, "cat").setOrigin(0, 0);
+        this.bird = this.add.sprite(340, 545, "bird").setOrigin(0, 0).setAlpha(0.5);
+        this.squirrel = this.add.sprite(350, 400, "squirrel").setOrigin(0, 0);
         
-        // Answers
+        // Question
         this.text1 = this.add.text(630, 270, "Which one is true?" + "\n", {font: "50px Arial", fill: "black"});
 
-        //Button
-        const helloButton1 = this.add.text(590, 360, 'The cat is above the squirrel.', {font: "40px Arial", fill: "#black" });
-        helloButton1.setInteractive();
+        // Answers
+        this.answer_1 = this.add.text(580, 360, 'The cat is above the squirrel.', {font: "40px Arial", fill: "black" });
+        this.answer_1.setInteractive();
 
-        const helloButton2 = this.add.text(590, 420, 'The cat is below the squirrel.', {font: "40px Arial", fill: "#black" });
-        helloButton2.setInteractive();
-        
+        this.answer_2 = this.add.text(580, 420, 'The cat is below the squirrel.', {font: "40px Arial", fill: "black" });
+        this.answer_2.setInteractive();
+
+        this.ok1 = this.add.sprite(1105, 362, "ok1").setOrigin(0, 0)
+
+        this.ok1.setInteractive().on('pointerdown', () => {
+
+            this.destroyBall()
+            this.ok1.destroy()
+            this.ok2.destroy()
+            this.phase_2()
+
+        });
+
+        this.ok2 = this.add.sprite(1105, 420, "ok2").setOrigin(0, 0);
+
+        this.ok2.setInteractive().on('pointerdown', () => {
+
+            this.ok2.setTint('red')
+
+        })
+
+        this.input.on('gameojectover', function(pointer, gameObject) {
+
+            gameObject.setTint(0x8EEDE2)
+
+        })
+
+        this.input.on('gameobjectout', function(pointer, gameObject) {
+
+            gameObject.clearTint()
+
+        })
+
+
     }
-    update(){
 
+    phase_2(){
+        
+        this.cat.setAlpha(0.5)
+        this.bird.setAlpha(1)
+        this.answer_1.setText('The bird is above the squirrel.');
+        this.answer_2.setText('The bird is below the squirrel.'); 
+
+        var ok1 = this.add.sprite(1105, 362, "ok1").setOrigin(0, 0)
+
+        ok1.setInteractive().on('pointerdown', () => {
+
+            ok1.setTint('red')
+
+        })
+
+        var ok2 = this.add.sprite(1105, 420, "ok2").setOrigin(0, 0);
+
+       
+        ok2.setInteractive().on('pointerdown', () => {
+
+            this.destroyBall()
+            ok1.destroy()
+            ok2.destroy()
+            //this.phase_2()
+
+        });
+
+        this.input.on('gameojectover', function(pointer, gameObject) {
+
+            gameObject.setTint(0x8EEDE2)
+
+        })
+
+        this.input.on('gameobjectout', function(pointer, gameObject) {
+
+            gameObject.clearTint()
+
+        })
+
+    }
+    
+    destroyBall(){
+
+        arr[ballNumber2 - 1].destroy()
+        ballNumber2 --
+
+    }
+
+    update() {
+
+        
     }
 
 }
